c3ccacda67b565200ac167c77fe0a1d228fd452a
Anatoly Rozhkov <arozhkov@7-tech.io>
2025-01-28 07:58:22 +0300

    Initial commit

---
+++ project/settings/dev.py
@@ -0,0 +1,19 @@
+from .common import *  # noqa
+from .links import *  # noqa
+
+SITE_URL = "http://localhost:8000"
+ALLOWED_HOSTS = ["*"]
+DEBUG = True
+SECRET_KEY = "not-a-valid-secret-key"
+
+MIDDLEWARE.append(
+    "livereload.middleware.LiveReloadScript",
+)
+
+INSTALLED_APPS += (
+    "livereload",
+)
+INTERNAL_IPS = [
+    "127.0.0.1",
+    "localhost",
+]
