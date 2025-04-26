0021f5aff81a220598dc93bcba9a15df71c7b922
Anatoly Rozhkov <arozhkov@7-tech.io>
2025-04-26 09:53:20 +0300

    Added model class meta

--- project/apps/projects/models/project_models.py
+++ project/apps/projects/models/project_models.py
@@ -10,8 +10,18 @@ class Project(UUIDModel, Named, CreatedModel):
     description = models.TextField("Description", max_length=600)
     due_date = models.DateField("Due date")
 
+    class Meta:
+        ordering = ("-created_at",)
+        verbose_name = "Project"
+        verbose_name_plural = "Projects"
+
 
 class Task(UUIDModel, Named, CreatedModel):
     project = models.ForeignKey(
         Project, verbose_name="Project", related_name="tasks", on_delete=models.CASCADE, null=True
     )
+
+    class Meta:
+        ordering = ("-created_at",)
+        verbose_name = "Task"
+        verbose_name_plural = "Tasks"
